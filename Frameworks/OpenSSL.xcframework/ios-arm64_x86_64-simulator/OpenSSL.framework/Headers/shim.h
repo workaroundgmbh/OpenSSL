b92e154e7da1a28df37610d9c834624a88a2719f
Vlastimir <vlastimir.radojevic@proglove.de>
2023-12-22 14:34:12 +0100

    reverting to v1.1.2 because v3+ has some problems with setting the password for certificate

--- Frameworks/OpenSSL.xcframework/ios-arm64_x86_64-simulator/OpenSSL.framework/Headers/shim.h
+++ Frameworks/OpenSSL.xcframework/ios-arm64_x86_64-simulator/OpenSSL.framework/Headers/shim.h
@@ -1,36 +1,36 @@
 #ifndef OpenSSLShim_h
 #define OpenSSLShim_h
 
-#include <OpenSSL/conf.h>
-#include <OpenSSL/evp.h>
-#include <OpenSSL/err.h>
-#include <OpenSSL/bio.h>
-#include <OpenSSL/x509.h>
-#include <OpenSSL/cms.h>
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/bio.h>
+#include <openssl/x509.h>
+#include <openssl/cms.h>
 
 #undef SSL_library_init
-static inline void SSL_library_init(void) {
+static inline void SSL_library_init() {
     OPENSSL_init_ssl(0, NULL);
 }
 
 #undef SSL_load_error_strings
-static inline void SSL_load_error_strings(void) {
+static inline void SSL_load_error_strings() {
     OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS \
                      | OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
 }
 
 #undef OpenSSL_add_all_ciphers
-static inline void OpenSSL_add_all_ciphers(void) {
+static inline void OpenSSL_add_all_ciphers() {
     OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_CIPHERS, NULL);
 }
 
 #undef OpenSSL_add_all_digests
-static inline void OpenSSL_add_all_digests(void) {
+static inline void OpenSSL_add_all_digests() {
     OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_DIGESTS, NULL);
 }
 
 #undef OpenSSL_add_all_algorithms
-static inline void OpenSSL_add_all_algorithms(void) {
+static inline void OpenSSL_add_all_algorithms() {
     #ifdef OPENSSL_LOAD_CONF
     OPENSSL_add_all_algorithms_conf();
     #else
